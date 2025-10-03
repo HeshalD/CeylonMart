44cb70d86a6208ed665e8c49ad8ee9747646f09c
Kaveesha Divyanjali <kaveeshadivyanjali2468@gmail.com>
2025-10-03 23:03:27 +0530

    pull

--- frontend/src/Pages/CartPage.jsx
+++ frontend/src/Pages/CartPage.jsx
@@ -127,7 +127,7 @@ export default function CartPage({ customerId }) {
               Clear Cart
             </button>
             <Link
-              to="/"
+              to="/Shop"
               className="px-4 py-2 text-white rounded-lg shadow bg-emerald-600 hover:bg-emerald-700"
             >
               Add Items
